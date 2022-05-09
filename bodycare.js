efd607c398dc6d680214d14025707e167e736a82
Harshal Nikhade <nikhadeharshu@gmail.com>
2022-05-09 14:15:11 +0530

    final file

---
+++ bodycare.js
@@ -0,0 +1,366 @@
+var Alldata = [{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Fragrance",
+  name: "Butterfly",
+  com: "Fine Fragrance Mist",
+  price: 175,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw1ee09012/crop/026439896_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Fragrance",
+  name: "Rose",
+  com: "Fine Fragrance Mist",
+  price: 128,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw126df1ff/crop/026465582_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Fragrance",
+  name: "Stone",
+  com: "Bar Soap",
+  price: 275,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwd4185b82/crop/026447063_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Fragrance",
+  name: "Happy Vibes",
+  com: "Bar Soap",
+  price: 365,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw7c9be460/crop/026465756_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Body Cream",
+  name: "Bubbly Rose",
+  com: "Woodsy",
+  price: 245,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw06129964/crop/026451891_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Body Cream",
+  name: "Butterfly",
+  com: "Woodsy",
+  price: 415,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw79c0ac80/crop/026468712_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Body Cream",
+  name: "Butterfly",
+  com: "Woodsy",
+  price: 197,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw79c0ac80/crop/026468712_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand:"Woodsy",
+  name:"Gingham",
+  com: "Sweety",
+  price: 99,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw15f7ab5a/crop/026465577_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Body ButtBody Cream",
+  name: "Stone",
+  com: "Sweety",
+  price: 175,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw81522767/crop/026401914_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Body ButtBody Cream",
+  name: "Rose",
+  com: "Sweety",
+  price: 125,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw0aac8b37/crop/026447051_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Body ButtBody Cream",
+  name: "Stone",
+  com: "Fruity",
+  price: 325,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwb7a96cc4/crop/026470500_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Body ButtBody Cream",
+  name: "Butterfly",
+  com: "Fruity",
+  price: 195,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwff743235/crop/026438530_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Soak",
+  name: "Black Tie",
+  com: "Fruity",
+  price: 199,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwe52c7f04/crop/026403504_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Soak",
+  name: "Cherry",
+  com: "Fruity",
+  price: 125,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw3bb780e2/crop/026447067_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Soak",
+  name: "Cherry",
+  com: "Fresh",
+  price: 135,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwb04aa57b/crop/026459592_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Soak",
+  name: "Almond",
+  com: "Fresh",
+  price: 425,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwbfc18541/crop/026401907_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Fragrance",
+  name: "Almond",
+  com: "Fresh",
+  price: 145,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwd097b2cd/crop/026451889_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Fizzy",
+  name: "Bourbon",
+  com: "Floral",
+  price: 139,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw3e419c30/crop/026438526_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Fizzy",
+  name: "Bourbon",
+  com: "Floral",
+  price: 217,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw744d9e74/crop/026414637_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Fizzy",
+  name: "Butterfly",
+  com: "Floral",
+  price: 314,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw170d959f/crop/026413789_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Fizzy",
+  name: "Butterfly",
+  com: "Floral",
+  price: 969,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw50a09db0/crop/026395407_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Accessories",
+  name: "Bubbly Rose",
+  com: "Citrus",
+  price: 124,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw0b5bbf2c/crop/026398798_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Accessories",
+  name: "Cherry",
+  com: "Citrus",
+  price: 75,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw2928b4c4/crop/026451871_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Bath Accessories",
+  name: "Gingham",
+  com: "Citrus",
+  price: 152,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw7e5f24d0/crop/026403502_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Fragrance",
+  name: "Almond",
+  com: "Citrus",
+  price: 149,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc7b40597/crop/026447068_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Fragrance",
+  name: "Happy Vibes",
+  com: "Citrus",
+  price: 231,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw5ed672f7/crop/026439892_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Fragrance",
+  name: "Rose",
+  com: "Fine Fragrance Mist",
+  price: 999,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+{
+  image_url: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw4751c03b/crop/026461149_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+  brand: "Fragrance",
+  name: "Bubbly Rose",
+  com: "Fine Fragrance Mist",
+  price: 525,
+  view: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+},
+
+
+
+];
+
+var container = JSON.parse(localStorage.getItem("container")) || [];
+displayData(Alldata);
+
+function displayData(data) {
+  document.querySelector("section").innerHTML = ""
+  data.forEach(function (elem, index) {
+    var main_box = document.createElement("div");
+    main_box.setAttribute("id", "head")
+
+
+    var img_div = document.createElement("div");
+    img_div.setAttribute("id", "main-box")
+    var img = document.createElement("img");
+    
+
+
+
+    var div1 = document.createElement("div");
+    div1.setAttribute("id", "div1");
+
+    var brd = document.createElement("p");
+    brd.setAttribute("class", "start")
+    var name = document.createElement("p");
+    name.setAttribute("class", "name")
+    var company = document.createElement("p");
+    company.setAttribute("class", "company")
+
+
+    var div2 = document.createElement("div");
+    div2.setAttribute("id", "div2");
+    var price = document.createElement("p");
+    price.setAttribute("class", "price");
+
+
+    var view = document.createElement("p");
+    view.setAttribute("class", "view");
+
+    var btn = document.createElement("button");
+    btn.setAttribute("id", "button")
+    btn.innerText = "ADD TO BAG";
+    btn.addEventListener("click", function () {
+      addToCart(ele)
+    })
+
+
+
+
+    img.setAttribute("src", elem.image_url);
+    img_div.append(img);
+
+    brd.innerText = elem.brand;
+    name.innerText = elem.name;
+    company.innerText = elem.com;
+    div1.append(brd, name, company);
+
+    price.innerText = "$" + elem.price;
+    view.innerText = elem.view;
+    div2.append(price, view);
+
+    main_box.append(img_div, div1, div2, btn);
+
+    document.querySelector("section").append(main_box);
+
+
+  })
+
+
+
+  function addToCart(ele) {
+    container.push(ele);
+    localStorage.setItem("container", JSON.stringify(container))
+
+  }
+}
+
+
+document.querySelector("#selectProducts2").addEventListener("change", filterCategory)
+function filterCategory() {
+  var selected = document.querySelector("#selectProducts2").value
+  if (selected == "FRAGRANCE NAME") {
+    window.location.reload()
+  }
+  else {
+    var filtered = Alldata.filter(function (elem) {
+      return elem.name == selected;
+    });
+    displayData(filtered)
+  }
+}
+document.querySelector("#selectProducts3").addEventListener("change", filterCategoryy)
+function filterCategoryy() {
+  var selected = document.querySelector("#selectProducts3").value
+  if (selected == "FRAGRANCE CATEGORY") {
+    window.location.reload()
+  }
+  else {
+    var filtered = Alldata.filter(function (elem) {
+      return elem.com == selected;
+    });
+    displayData(filtered)
+  }
+}
+document.querySelector("#selectProducts1").addEventListener("change", filterCategoryyy)
+function filterCategoryyy() {
+  var selected = document.querySelector("#selectProducts1").value
+  if (selected == "PRODUCT TYPE") {
+    window.location.reload()
+  }
+  else {
+    var filtered = Alldata.filter(function (elem) {
+      return elem.brand == selected;
+    });
+    displayData(filtered)
+  }
+}
+
+
+document.querySelector("#sort").addEventListener("change", sortPrice)
+    function sortPrice() {
+        var selected = document.querySelector("#sort").value
+        if(selected == "none"){
+            window.location.reload()
+        }
+        else if (selected == "High to low") {
+            Alldata.sort(function (a, b) {
+                return b.price - a.price
+            })
+            displayData(Alldata)
+        }
+           else if (selected == "Low to high") {
+            Alldata.sort(function (a, b) {
+                return a.price - b.price
+            })
+            displayData(Alldata)
+        }
+    }
